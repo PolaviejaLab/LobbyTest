71a291712a30f647547a888fe0f17d1a766f4983
Ivar Clemens <post@ivarclemens.nl>
2016-02-29 15:30:02 +0000

    Show connect and disconnect events in GUI.

--- Assets/Lobby/Scripts/WatchedNetworkManager.cs
+++ Assets/Lobby/Scripts/WatchedNetworkManager.cs
@@ -2,20 +2,26 @@
 using UnityEngine.Events;
 using UnityEngine.Networking;
 
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
+[Serializable]
 public class ServerConnectEvent: UnityEvent<NetworkConnection>
 {
 }
 
+[Serializable]
 public class ServerDisconnectEvent: UnityEvent<NetworkConnection>
 {
 }
 
 public class WatchedNetworkManager : NetworkManager 
 {
+    [SerializeField]
     public ServerConnectEvent ServerConnect = new ServerConnectEvent();
+
+    [SerializeField]
     public ServerDisconnectEvent ServerDisconnect = new ServerDisconnectEvent();
 
     public List<NetworkConnection> connections = new List<NetworkConnection>();
