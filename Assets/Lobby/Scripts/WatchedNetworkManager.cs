ca455e2f3bdccf2de93cdab5750719400a9b0928
Ivar Clemens <post@ivarclemens.nl>
2016-03-02 14:02:30 +0000

    Added client events.

--- Assets/Lobby/Scripts/WatchedNetworkManager.cs
+++ Assets/Lobby/Scripts/WatchedNetworkManager.cs
@@ -1,30 +1,110 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
 
 using System;
 using System.Collections;
 using System.Collections.Generic;
 
-[Serializable]
-public class ServerConnectEvent: UnityEvent<NetworkConnection>
-{
-}
+[Serializable] public class ClientConnectEvent: UnityEvent<NetworkConnection> { }
+[Serializable] public class ClientDisconnectEvent: UnityEvent<NetworkConnection> { }
+[Serializable] public class ClientErrorEvent: UnityEvent<NetworkConnection, int> { }
+[Serializable] public class ClientNotReadyEvent: UnityEvent<NetworkConnection> { }
+[Serializable] public class ClientSceneChangedEvent: UnityEvent<NetworkConnection> { }
 
-[Serializable]
-public class ServerDisconnectEvent: UnityEvent<NetworkConnection>
-{
-}
+[Serializable] public class MatchCreateEvent: UnityEvent<CreateMatchResponse> { }
+[Serializable] public class MatchListEvent: UnityEvent<ListMatchResponse> { }
+
+[Serializable] public class ServerConnectEvent: UnityEvent<NetworkConnection> { }
+[Serializable] public class ServerDisconnectEvent: UnityEvent<NetworkConnection> { }
+
+
 
 public class WatchedNetworkManager : NetworkManager 
 {
-    [SerializeField]
-    public ServerConnectEvent ServerConnect = new ServerConnectEvent();
+    // Called on the client when connected to a server.
+    [SerializeField] public ClientConnectEvent ClientConnect = new ClientConnectEvent();
 
-    [SerializeField]
-    public ServerDisconnectEvent ServerDisconnect = new ServerDisconnectEvent();
+    // Called on clients when disconnected from a server.
+    [SerializeField] public ClientDisconnectEvent ClientDisconnect = new ClientDisconnectEvent();
+
+    // Called on clients when a network error occurs.
+    [SerializeField] public ClientErrorEvent ClientError = new ClientErrorEvent();
+
+    // Called on clients when a servers tells the client it is no longer ready.
+    [SerializeField] public ClientNotReadyEvent ClientNotReady = new ClientNotReadyEvent();
+
+    // Called on clients when a scene has completed loaded, when the scene load was initiated by the server.
+    [SerializeField] public ClientSceneChangedEvent ClientSceneChanged = new ClientSceneChangedEvent();
+
+
+    // This is invoked when a match has been created.
+    [SerializeField] public MatchCreateEvent MatchCreate = new MatchCreateEvent();
+
+    // This is invoked when a list of matches is returned from ListMatches().
+    [SerializeField] public MatchListEvent MatchList = new MatchListEvent();
+
+
+    // Called on the server when a new client connects.
+    [SerializeField] public ServerConnectEvent ServerConnect = new ServerConnectEvent();
+
+    // Called on the server when a client disconnects.
+    [SerializeField] public ServerDisconnectEvent ServerDisconnect = new ServerDisconnectEvent();
 
     public List<NetworkConnection> connections = new List<NetworkConnection>();
+
+
+
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        base.OnClientConnect(conn);
+        ClientConnect.Invoke(conn);
+    }
+
+
+    public override void OnClientDisconnect(NetworkConnection conn)
+    {
+        ClientDisconnect.Invoke(conn);
+        base.OnClientDisconnect(conn);
+    }
+
+
+    public override void OnClientError(NetworkConnection conn, int errorCode)
+    {
+        base.OnClientError(conn, errorCode);
+        ClientError.Invoke(conn, errorCode);
+    }
+
+
+    public override void OnClientNotReady(NetworkConnection conn)
+    {
+        base.OnClientNotReady(conn);
+        ClientNotReady.Invoke(conn);
+    }
+
+
+    public override void OnClientSceneChanged(NetworkConnection conn)
+    {
+        base.OnClientSceneChanged(conn);
+        ClientSceneChanged.Invoke(conn);
+    }
+
+
+    public override void OnMatchCreate(CreateMatchResponse matchInfo)
+    {
+        base.OnMatchCreate(matchInfo);
+        MatchCreate.Invoke(matchInfo);
+    }
+
+
+    public override void OnMatchList(ListMatchResponse matchList)
+    {
+        base.OnMatchList(matchList);
+        MatchList.Invoke(matchList);
+    }
+
 
     public override void OnServerConnect(NetworkConnection conn)
     {
@@ -33,6 +113,7 @@ public class WatchedNetworkManager : NetworkManager
         connections.Add(conn);
         ServerConnect.Invoke(conn);
     }
+
 
     public override void OnServerDisconnect(NetworkConnection conn)
     {
